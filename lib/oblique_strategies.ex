8c24a3085b7bb6a74d65b6f264f1975f61e46746
glhrmv <glhrmv@icloud.com>
2025-01-27 20:33:37 +0000

    add basic functionality and tests

--- lib/oblique_strategies.ex
+++ lib/oblique_strategies.ex
@@ -1,18 +1,45 @@
 defmodule ObliqueStrategies do
   @moduledoc """
-  Documentation for `ObliqueStrategies`.
+  Provides access to the Oblique Strategies cards.
   """
+
+  @cards File.read!(
+           Path.join(
+             :code.priv_dir(:oblique_strategies),
+             "strategies.json"
+           )
+         )
+         |> JSON.decode!()
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ObliqueStrategies.hello()
-      :world
-
+  Returns all strategies as a list of strings.
   """
-  def hello do
-    :world
+  def all do
+    @cards
+  end
+
+  @doc """
+  Draws a random strategy from the list.
+  """
+  def draw do
+    Enum.random(@cards)
+  end
+
+  @doc """
+  Returns the preamble (instructions) card.
+  """
+  def preamble do
+    ~s"""
+    These cards evolved from our separate observations on the principles \
+    underlying what we were doing. Sometimes they were recognized in retrospect \
+    (intellect catching up with intuition), sometimes they were identified as \
+    they were happening, sometimes they were formulated.
+
+    They can be used as a pack (a set of possibilities being continuously reviewed \
+    in the mind) or by drawing a single card from the shuffled pack when a dilemma \
+    occurs in a working situation. In this case, the card is trusted even if its \
+    appropriateness is quite unclear. They are not final, as new ideas will \
+    present themselves, and others will become self-evident.
+    """
   end
 end
--- test/oblique_strategies_test.exs
+++ test/oblique_strategies_test.exs
@@ -2,7 +2,26 @@ defmodule ObliqueStrategiesTest do
   use ExUnit.Case
   doctest ObliqueStrategies
 
-  test "greets the world" do
-    assert ObliqueStrategies.hello() == :world
+  test "all/0 returns all strategies as a non-empty list of strings" do
+    strategies = ObliqueStrategies.all()
+
+    assert is_list(strategies)
+    assert length(strategies) > 0
+    assert Enum.all?(strategies, &is_binary/1)
+  end
+
+  test "draw/0 returns a random strategy from the list" do
+    strategy = ObliqueStrategies.draw()
+
+    assert is_binary(strategy)
+    assert strategy in ObliqueStrategies.all()
+  end
+
+  test "preamble/0 contains key phrases" do
+    preamble = ObliqueStrategies.preamble()
+
+    assert String.contains?(preamble, "These cards evolved")
+    assert String.contains?(preamble, "principles underlying")
+    assert String.contains?(preamble, "They are not final")
   end
 end
